e1f4d3a669ef3433a6314ad2904b0c304cabf95c
Daniel <yourmediasoft@gmail.com>
2023-02-21 18:03:20 -0500

    Apply hasMany relation with posts function in Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
