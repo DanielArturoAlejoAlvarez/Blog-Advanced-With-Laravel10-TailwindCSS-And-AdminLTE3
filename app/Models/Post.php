9974631e62510c620a2da6e445e110ae0ae3c7f4
Daniel <yourmediasoft@gmail.com>
2023-02-21 18:13:42 -0500

    Apply belongsToMany relations with tags function in Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,4 +19,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
