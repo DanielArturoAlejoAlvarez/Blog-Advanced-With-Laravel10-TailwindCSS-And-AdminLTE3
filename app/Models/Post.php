c949f7e81813d11e4b081d5dfd166564a583492a
Daniel <yourmediasoft@gmail.com>
2023-02-21 18:08:15 -0500

    Apply belongsTo relations with category and user in Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
