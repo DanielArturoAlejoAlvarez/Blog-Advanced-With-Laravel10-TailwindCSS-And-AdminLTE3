0e4dbc0fa7b46ac78f041c0fd571b4a638998b7d
Daniel <yourmediasoft@gmail.com>
2023-02-22 10:03:36 -0500

    Apply from call DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Category;
-use App\Models\Post;
+//use App\Models\Post;
 use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         Category::factory(4)->create();
         Tag::factory(8)->create();
-        
+        $this->call(PostSeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
