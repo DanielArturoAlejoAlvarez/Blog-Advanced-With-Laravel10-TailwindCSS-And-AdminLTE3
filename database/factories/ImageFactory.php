db2666f395040b43c41d4761466808dae4e9a122
Daniel <yourmediasoft@gmail.com>
2023-02-22 09:37:33 -0500

    Creating a Tag Factory to test data

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
+ */
+class ImageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [];
+        // return [
+        //     'url'       =>      'posts/' . $this->faker->image('public/storage/posts', 640, 480, null, false),
+        // ];
+    }
+}
---
+++ database/factories/TagFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tag>
+ */
+class TagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->word(20);
+        return [
+            'name'     =>      $name,
+            'slug'      =>      Str::slug($name),
+        ];
+    }
+}
