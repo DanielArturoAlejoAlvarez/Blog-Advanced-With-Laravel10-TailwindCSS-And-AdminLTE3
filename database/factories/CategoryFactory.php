a2e492924f5e08c4e8a84803859f169f976683bd
Daniel <yourmediasoft@gmail.com>
2023-02-22 09:02:18 -0500

    Creating a Category Factory to test data

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->word(20);
+        return [
+            'title'     =>      $title,
+            'slug'      =>      Str::slug($title),
+        ];
+    }
+}
