6545618db8eded5028361bfa6f72f910c8da74e4
Daniel <yourmediasoft@gmail.com>
2023-02-22 10:00:50 -0500

    Creating Post Seeder to generate posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(UserSeeder::class);
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+        
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Image;
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $posts = Post::factory(100)->create();
+
+        foreach ($posts as $post) {
+            Image::factory(1)->create([
+                'imageable_id'  =>  $post->id,
+                'imageable_type'=>  Post::class,
+            ]);
+        }
+    }
+}
