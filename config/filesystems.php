05d034a53f9a6db4393064fb9c148bb7d3aefb44
Daniel <yourmediasoft@gmail.com>
2023-02-24 13:04:30 -0500

    Config events and components with livewire alpine in our menu navigation

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'public'),//local
+    'default' => env('FILESYSTEM_DISK', 'local'),
 
     /*
     |--------------------------------------------------------------------------
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
+        //Storage::deleteDirectory('posts');
         //Storage::makeDirectory('posts');
 
         $this->call(UserSeeder::class);
