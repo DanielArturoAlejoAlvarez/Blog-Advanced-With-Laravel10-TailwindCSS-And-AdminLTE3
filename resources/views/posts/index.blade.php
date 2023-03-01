db926732934dc2cd72ca0cadd996becaf2a53190
Daniel <yourmediasoft@gmail.com>
2023-03-01 10:24:41 -0500

    Refactoring our views and routes

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,33 +1,33 @@
 <x-app-layout>
 
 
-<div class="container py-8 px-8">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        @foreach ($posts as $post)
-            <article class="@if ($loop->first)
-                  md:col-span-2  
-                @endif w-full h-80 bg-center bg-cover" style="background-image:url({{Storage::url($post->image->url)}})">
-               <div class="w-full h-full px-8 flex flex-col justify-center">
-                    <div>
-                        @foreach ($post->tags as $tag)
-                            <a style="background-color:{{ $tag->colour}};" class="inline-block bg-gray-600 @if ($tag->colour == 'yellow')
-                               text-gray-800 
-                            @else 
-                            text-white
-                            @endif
-                             h-6 px-3 rounded-full" href="">{{$tag->name}}</a>
-                        @endforeach
+    <div class="container py-8 px-8">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            @foreach ($posts as $post)
+                <article class="@if ($loop->first) md:col-span-2 @endif w-full h-80 bg-center bg-cover"
+                    style="background-image:url({{ Storage::url($post->image->url) }})">
+                    <div class=" w-full h-full px-8 flex flex-col justify-center">
+                        <div>
+                            @foreach ($post->tags as $tag)
+                                <a href="{{ route('posts.tag', $tag) }}" style="background-color:{{ $tag->colour }};"
+                                    class="inline-block bg-gray-600 
+                                @if ($tag->colour == 'yellow') text-gray-800 
+                                @else text-white 
+                                @endif h-6 px-3 rounded-full">{{ $tag->name }}</a>
+                            @endforeach
+                        </div>
+                        <h1 class="text-4xl mt-2 text-white leading-8 font-bold"><a
+                                href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
+                        </h1>
                     </div>
-                    <h1 class="text-4xl text-white leading-8 font-bold"><a href="{{route('posts.show', $post)}}">{{$post->title}}</a></h1>
-               </div>
-            </article> 
-        @endforeach
-    </div>
+                </article>
+            @endforeach
+        </div>
 
-    <div class="mt-4">
-        {{$posts->links()}}
+        <div class="mt-4">
+            {{ $posts->links() }}
+        </div>
     </div>
-</div>
 
 
 </x-app-layout>
