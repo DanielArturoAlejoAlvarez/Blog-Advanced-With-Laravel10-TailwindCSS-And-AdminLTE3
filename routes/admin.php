65e77fc336d111ef6766ae6144e7b83bff81630f
Daniel <yourmediasoft@gmail.com>
2023-03-02 20:16:58 -0500

    Creating PostController admin and adding routes

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\TagController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,5 @@ Route::get('', [HomeController::class, 'index'])->name('admin.home');
 Route::resource('categories', CategoryController::class)->names('admin.categories');
 
 Route::resource('tags', TagController::class)->names('admin.tags');
+
+Route::resource('posts', PostController::class)->names('admin.posts');
