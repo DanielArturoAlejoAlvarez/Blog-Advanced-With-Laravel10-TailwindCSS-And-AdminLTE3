cd51a07e2115f29785af80fee2a228a4c8ae032c
Daniel <yourmediasoft@gmail.com>
2023-03-08 10:00:38 -0500

    Creating RoleSeeder and config Role model

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+//Role model from Spatie-Permission
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
