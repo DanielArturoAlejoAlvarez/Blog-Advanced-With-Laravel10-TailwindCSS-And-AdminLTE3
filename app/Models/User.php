56afb87f555ed576d2ad42829cf34ce1c3b40495
Daniel <yourmediasoft@gmail.com>
2023-03-08 09:56:28 -0500

    Config permissions asign roles from User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,9 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
+//Spatie-Permissions Roles of users
+use Spatie\Permission\Traits\HasRoles;
+
 class User extends Authenticatable
 {
     use HasApiTokens;
@@ -17,6 +20,8 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
