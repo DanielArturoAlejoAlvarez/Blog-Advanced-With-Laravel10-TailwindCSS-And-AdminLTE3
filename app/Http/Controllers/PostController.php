15d8274def5815ee54976b12cfb1a34c81c7898d
Daniel <yourmediasoft@gmail.com>
2023-03-11 18:30:53 -0500

    Using the Cache facade to store our posts in memory

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,11 +7,21 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Tag;
 
+use Illuminate\Support\Facades\Cache;
+
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::where('status',2)->latest('id')->paginate(8);
+        //Using Cache Facade
+        if (Cache::has('posts')) {
+            $posts = Cache::get('posts');
+        }else{
+            $posts = Post::where('status',2)->latest('id')->paginate(8);
+            Cache::put('posts',$posts);
+        }
+
+        //$posts = Post::where('status',2)->latest('id')->paginate(8);
         return view('posts.index', compact('posts'));
     }
 
@@ -19,34 +29,33 @@ class PostController extends Controller
     {
         $this->authorize('published', $post);
         //return $post;
-        $filtered = Post::where('category_id',$post->category_id)
-                            ->where('status',2)
-                            ->where('id','!=',$post->id)
-                            ->latest('id')
-                            ->take(4)
-                            ->get();
+        $filtered = Post::where('category_id', $post->category_id)
+            ->where('status', 2)
+            ->where('id', '!=', $post->id)
+            ->latest('id')
+            ->take(4)
+            ->get();
 
-        return view('posts.show', compact('post','filtered'));
+        return view('posts.show', compact('post', 'filtered'));
     }
 
     public function category(Category $category)
     {
-        $posts = Post::where('category_id',$category->id)
-                        ->where('status',2)
-                        ->latest('id')
-                        ->paginate(6);
+        $posts = Post::where('category_id', $category->id)
+            ->where('status', 2)
+            ->latest('id')
+            ->paginate(6);
 
-        return view('posts.category', compact('posts','category'));
+        return view('posts.category', compact('posts', 'category'));
         //return $category;
     }
 
     public function tag(Tag $tag)
     {
-        $posts = $tag->posts()->where('status',2)
-                    ->latest('id')
-                    ->paginate(4);
-        return view('posts.tag', compact('posts','tag'));
+        $posts = $tag->posts()->where('status', 2)
+            ->latest('id')
+            ->paginate(4);
+        return view('posts.tag', compact('posts', 'tag'));
         //return $tag;
     }
-
 }
